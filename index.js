5cb98ada1652d97e86794d022eaf3e2f04c112fd
keval.trivedi <keval.trivedi@estatic-infotech.com>
2026-02-20 10:14:29 +0530

    dat

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ server.use((req, res, next) => {
   // Only validate for POST/PUT/PATCH requests
   if ((req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') && req.body && req.body.name) {
     if (req.body.name.length > 20) {
-      return res.status(400).send("Name cannot exceed 40 characters.");
+      return res.status(400).send("Name cannot exceed 20 characters.");
     }
-    else if(res.body.username.length >15 ){
+    if(res.body.username.length >15 ){
         return res.status(400).send("Username cannot exceed 15 character.")
     }
-    else if(res.body.email.length > 25 ){
+    if(res.body.email.length > 25 ){
         return res.status(400).send("Email cannot exceed 15 character")
     }
-    else if(res.body.phone.length > 15){
+    if(res.body.phone.length > 15){
         return res.status(400).send("Phone Numbers exceed 15 character ")
     }
-    else if(res.body.company.length > 20){
+    if(res.body.company.length > 20){
         return res.status(400).send("Company length exceed 20 character")
     }
   }
